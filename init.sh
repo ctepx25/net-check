b40278d8c65ae940e48caec01ff6209cdc908abd
ctepx25 <40763327+ctepx25@users.noreply.github.com>
2021-05-06 11:56:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 service nginx start
-case $1 in
-  3044)
-    nc -lvk 3044
-    ;;
-  4044)
-    nc -lvk 4044
-    ;;
-  *)
-    nc -lvk 3044 & nc -lvk 4044
-    ;;
-esac
+[[ ! -z $1 ]] && nc -lvk $1 || (nc -lvk 3044 & nc -lvk 4044)
